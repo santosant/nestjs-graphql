35061927316336cbf5e384f7b246e942979d7fc7
santosant <hiroto.anderson@gmail.com>
2021-10-26 19:33:48 -0300

    feat(inital): initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    TypeOrmModule.forRoot({}),
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
